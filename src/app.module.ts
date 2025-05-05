03922092ba9b5cd0ba0738cc0a1f2fe2f88ad256
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-05-05 16:58:31 +0700

    su dung mongodb voi nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://tranvanhung2003:Hungtran2003.@cluster0.xsoyzi9.mongodb.net/',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
