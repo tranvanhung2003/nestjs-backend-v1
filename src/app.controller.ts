efabfeb7f8f2980d5b6da78f0a4667dae6b7a24a
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-05-15 00:43:37 +0700

    localguard voi passport

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,14 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  @Render('home')
-  handleHomepage() {
-    const message = this.appService.getHello();
-
-    return { message };
+  @UseGuards(LocalAuthGuard)
+  @Post('/login')
+  handleLogin(@Request() req) {
+    return req.user;
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,20 +5,20 @@ import { UsersService } from 'src/users/users.service';
 export class AuthService {
   constructor(private userService: UsersService) {}
 
-  async validateUser(username: string, pass: string): Promise<any> {
+  async validateUser(username: string, password: string): Promise<any> {
     const user = await this.userService.findOneByUsername(username);
 
     if (!user) {
       return null;
     }
 
-    const isValid = this.userService.isValidPassword(pass, user.password);
+    const isValid = this.userService.isValidPassword(password, user.password);
 
     if (!isValid) {
       return null;
     }
 
-    const { password, ...result } = user;
+    const { password: _, ...result } = user.toObject();
     return result;
   }
 }
