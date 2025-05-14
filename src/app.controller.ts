0fe6ecba45532a588d5642c2faa0edd502b3e3e2
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-05-15 00:47:03 +0700

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,7 +7,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @UseGuards(LocalAuthGuard)
-  @Post('/login')
+  @Post('login')
   handleLogin(@Request() req) {
     return req.user;
   }
