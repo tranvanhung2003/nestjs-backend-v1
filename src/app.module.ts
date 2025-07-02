4e6874326f409bc793bbb571d29ce8892754abdd
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-07-02 16:10:10 +0700

    su dung MongoDB voi NestJS

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/mydatabase')],
   controllers: [AppController],
   providers: [AppService],
 })
