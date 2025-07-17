4f6518b0b529e6e5ce01b2de3bef3f350997e7c7
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-07-17 22:22:33 +0700

    LocalGuard voi Passport

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,14 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Render,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
@@ -14,5 +22,11 @@ export class AppController {
   handleHomePage() {
     const message = this.appService.getHello();
     return { message };
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('/login')
+  handleLogin(@Request() req) {
+    return req.user;
   }
 }
