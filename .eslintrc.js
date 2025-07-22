d46a6c66767365ad34b9cb722f00ecacfefcb6d3
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-07-22 12:28:54 +0700

    update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,7 +24,7 @@ module.exports = {
     // custom configurations
     '@typescript-eslint/no-unused-vars': 'off',
     'prettier/prettier': [
-      'error',
+      'warn',
       {
         endOfLine: 'auto',
       },
