f1b72d6232cbba54d5c015c39f3f2e3c28255c95
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-08-12 16:13:50 +0700

    update user schema

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,25 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { Type } from 'class-transformer';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNotEmptyObject,
+  IsObject,
+  ValidateNested,
+} from 'class-validator';
+import mongoose from 'mongoose';
 
-export class CreateUserDto {
+class Company {
+  @IsNotEmpty()
+  _id: mongoose.Schema.Types.ObjectId;
+
+  @IsNotEmpty()
+  name: string;
+}
+
+export class RegisterUserDto {
+  @IsNotEmpty({ message: 'Name không được để trống' })
+  name: string;
+
   @IsEmail({}, { message: 'Email không đúng định dạng' })
   @IsNotEmpty({ message: 'Email không được để trống' })
   email: string;
@@ -8,7 +27,23 @@ export class CreateUserDto {
   @IsNotEmpty({ message: 'Password không được để trống' })
   password: string;
 
-  name: string;
+  @IsNotEmpty({ message: 'Age không được để trống' })
+  age: number;
 
+  @IsNotEmpty({ message: 'Gender không được để trống' })
+  gender: string;
+
+  @IsNotEmpty({ message: 'Address không được để trống' })
   address: string;
+}
+
+export class CreateUserDto extends RegisterUserDto {
+  @IsNotEmpty({ message: 'Role không được để trống' })
+  role: string;
+
+  @IsNotEmptyObject()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => Company)
+  company: Company;
 }
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,8 +1,9 @@
 import { OmitType } from '@nestjs/mapped-types';
+import mongoose from 'mongoose';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends OmitType(CreateUserDto, [
   'password',
 ] as const) {
-  _id: string;
+  _id: mongoose.Schema.Types.ObjectId;
 }
