3b9311d2748a55c7df3edaac427a114ba3a25987
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-10-19 12:57:45 +0700

    update dto

--- src/jobs/dto/create-job.dto.ts
+++ src/jobs/dto/create-job.dto.ts
@@ -1,1 +1,56 @@
-export class CreateJobDto {}
+import { Type } from 'class-transformer';
+import {
+  ArrayNotEmpty,
+  IsArray,
+  IsNotEmpty,
+  IsNotEmptyObject,
+  IsObject,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+import mongoose from 'mongoose';
+
+class Company {
+  @IsNotEmpty()
+  _id: mongoose.Schema.Types.ObjectId;
+
+  @IsNotEmpty()
+  name: string;
+}
+
+export class CreateJobDto {
+  @IsNotEmpty({ message: 'Name không được để trống' })
+  name: string;
+
+  @IsArray()
+  @ArrayNotEmpty()
+  @IsString({ each: true })
+  skills: string[];
+
+  @IsNotEmptyObject()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => Company)
+  company: Company;
+
+  @IsNotEmpty({ message: 'Salary không được để trống' })
+  salary: number;
+
+  @IsNotEmpty({ message: 'Quantity không được để trống' })
+  quantity: number;
+
+  @IsNotEmpty({ message: 'Level không được để trống' })
+  level: string;
+
+  @IsNotEmpty({ message: 'Description không được để trống' })
+  description: string;
+
+  @IsNotEmpty({ message: 'StartDate không được để trống' })
+  startDate: Date;
+
+  @IsNotEmpty({ message: 'EndDate không được để trống' })
+  endDate: Date;
+
+  @IsNotEmpty({ message: 'isActive không được để trống' })
+  isActive: boolean;
+}
