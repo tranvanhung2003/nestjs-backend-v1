71391aaed403c3548e6e064ffedc30dbc65b6672
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-10-24 15:37:02 +0700

    gioi thieu upload files

---
+++ src/files/files.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { UpdateFileDto } from './dto/update-file.dto';
+import { FilesService } from './files.service';
+
+@Controller('files')
+@UseInterceptors(FileInterceptor('file'))
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post('upload')
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
+  }
+
+  @Get()
+  findAll() {
+    return this.filesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.filesService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateFileDto: UpdateFileDto) {
+    return this.filesService.update(+id, updateFileDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.filesService.remove(+id);
+  }
+}
