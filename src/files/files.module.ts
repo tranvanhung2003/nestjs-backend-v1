a8a65769cd540a4bf85eb00086ef524c187967d8
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-10-24 23:09:18 +0700

    destination upload file (part 1)

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-import { FilesService } from './files.service';
+import { MulterModule } from '@nestjs/platform-express';
 import { FilesController } from './files.controller';
+import { FilesService } from './files.service';
+import { MulterConfigService } from './multer.config';
 
 @Module({
+  imports: [MulterModule.registerAsync({ useClass: MulterConfigService })],
   controllers: [FilesController],
-  providers: [FilesService]
+  providers: [FilesService],
 })
 export class FilesModule {}
