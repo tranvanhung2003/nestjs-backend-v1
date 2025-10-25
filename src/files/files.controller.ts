52e057ac10b84d94e6f23f8abe7cefcae6adb046
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-10-25 22:06:52 +0700

    update

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -23,7 +23,7 @@ export class FilesController {
   @Public()
   @Post('upload')
   @ResponseMessage('Upload single file')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(FileInterceptor('fileUpload'))
   uploadFile(
     @UploadedFile(
       new ParseFilePipeBuilder()
