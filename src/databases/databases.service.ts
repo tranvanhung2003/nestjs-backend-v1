73318c4972d8a0597dd31f81486c8316b68fb4d5
tranvanhung2003 <tranvanhan29042007@gmail.com>
2025-11-07 23:53:14 +0700

    bai tap tao fake data

--- src/databases/databases.service.ts
+++ src/databases/databases.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { InjectModel } from '@nestjs/mongoose';
 import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
@@ -9,9 +9,12 @@ import {
 import { Role, RoleDocument } from 'src/roles/schema/role.schema';
 import { User, UserDocument } from 'src/users/schemas/user.schema';
 import { UsersService } from 'src/users/users.service';
+import { ADMIN_ROLE, INIT_PERMISSIONS, USER_ROLE } from './sample';
 
 @Injectable()
 export class DatabasesService implements OnModuleInit {
+  private readonly logger = new Logger(DatabasesService.name);
+
   constructor(
     private readonly configService: ConfigService,
     private readonly usersService: UsersService,
@@ -23,7 +26,85 @@ export class DatabasesService implements OnModuleInit {
     private readonly roleModel: SoftDeleteModel<RoleDocument>,
   ) {}
 
-  onModuleInit() {
-    console.log('The module has been initialized.');
+  async onModuleInit() {
+    const isInit = this.configService.get<boolean>('SHOULD_INIT');
+
+    if (!isInit) {
+      return;
+    }
+
+    const countUser = await this.userModel.countDocuments();
+    const countPermission = await this.permissionModel.countDocuments();
+    const countRole = await this.roleModel.countDocuments();
+
+    // create permissions
+    if (countPermission === 0) {
+      await this.permissionModel.insertMany(INIT_PERMISSIONS);
+    }
+
+    // create roles
+    if (countRole === 0) {
+      const permissions = await this.permissionModel.find().select('_id');
+      await this.roleModel.insertMany([
+        {
+          name: ADMIN_ROLE,
+          description: 'Admin thì full quyền',
+          isActive: true,
+          permissions,
+        },
+        {
+          name: USER_ROLE,
+          description: 'Người dùng/ứng viên sử dụng hệ thống',
+          isActive: true,
+          permissions: [], // không set quyền, chỉ cần add role
+        },
+      ]);
+    }
+
+    // create users
+    if (countUser === 0) {
+      const adminRole = await this.roleModel.findOne({ name: ADMIN_ROLE });
+      const userRole = await this.roleModel.findOne({ name: USER_ROLE });
+
+      await this.userModel.insertMany([
+        {
+          name: "I'm admin",
+          email: 'admin@gmail.com',
+          password: this.usersService.getHashPassword(
+            this.configService.get<string>('INIT_PASSWORD'),
+          ),
+          age: 22,
+          gender: 'MALE',
+          address: 'VietNam',
+          role: adminRole?._id,
+        },
+        {
+          name: "I'm Trần Văn Hưng",
+          email: 'tranvanhung@gmail.com',
+          password: this.usersService.getHashPassword(
+            this.configService.get<string>('INIT_PASSWORD'),
+          ),
+          age: 22,
+          gender: 'MALE',
+          address: 'VietNam',
+          role: adminRole?._id,
+        },
+        {
+          name: "I'm normal user",
+          email: 'user@gmail.com',
+          password: this.usersService.getHashPassword(
+            this.configService.get<string>('INIT_PASSWORD'),
+          ),
+          age: 22,
+          gender: 'MALE',
+          address: 'VietNam',
+          role: userRole?._id,
+        },
+      ]);
+    }
+
+    if (countUser > 0 && countRole > 0 && countPermission > 0) {
+      this.logger.log('>>> ALREADY INIT SAMPLE DATA...');
+    }
   }
 }
